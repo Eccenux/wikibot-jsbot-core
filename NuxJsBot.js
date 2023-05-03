bc9d063c326528c385121f19ae7dace52dd08a72
eccenux <eccenux+git@gmail.com>
2023-05-04 00:28:18 +0200

    procedure

--- NuxJsBot.js
+++ NuxJsBot.js
@@ -4,13 +4,16 @@
  * (tags: jsbot, botjs, botskjs, js-bot)
  * 
  * Procedura:	
-	0. Włącz ładowanie jsbot.
+	0. (temp) Włącz ładowanie jsbot.
 	1. Zmiana w `prepareSk()` i test.
-	2. [[Specjalna:Uprawnienia/Nux]] (włącz [[Wikipedia:Użytkownicy o ukrytej aktywności]]).
-	3. monobook
+	2. (temp) [[Specjalna:Uprawnienia/Nux]] (włącz [[Wikipedia:Użytkownicy o ukrytej aktywności]]).
+	3. (temp) monobook
 	4. Przygotowanie wyszukiwania (linki do edycji): jsbotsk_search_prep();
+	5. (temp) Wyłączenie obrazków (reguły uBlock):
+		upload.wikimedia.org * image block
+		pl.wikipedia.org * image block
 	...
-	X. Zakomentuj ładowanie jsbot i zrewertuj zmiany konta.
+	X. Powrót tymczasowych zmian.
  * 
  * @see NuxJsBot.js (in varia-linter)
  */
