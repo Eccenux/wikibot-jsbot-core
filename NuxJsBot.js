fd7b985381d04aa601efe4f357f3c35a19dfa582
Nux <eccenux+git@gmail.com>
2023-05-22 04:11:52 +0200

    col-begin/end na wikiflex

--- NuxJsBot.js
+++ NuxJsBot.js
@@ -150,14 +150,23 @@
 			const m = text.match(re);
 			return m ? m.length : 0;
 		}
-		/** Podmiana pseudo-tabel. */
+		/** Podmiana pseudo-tabel na wikiflex. */
 		flexColumnTables(str) {
 			// zwijanie
 			var tables = new SkTableHide();
 
-			// [[Wikipedysta:Nux/test_sk_table_hide#Niby_kolumny]]
-			str = str.replace(/\n\{\|\n[|\-\n]*\n\|\s*(<tab<[0-9]+>tab>)\s*\|\s*(<tab<[0-9]+>tab>)\s*\n\|\}/g, '\n<div class="wikiflex">\n$1\n$2\n</div>');
+			str = tables.hide(str);
 
+			// [[Wikipedysta:Nux/test_sk_table_hide#Niby_kolumny]]
+			str = str.replace(/\n\{\|\n[|\-\n]*\n\|\s*(<tab<[0-9]+>tab>)\s*\|\s*(<tab<[0-9]+>tab>)\s*\n\|\}/g
+				, '\n<div class="wikiflex">\n$1\n$2\n</div>'
+			);
+
+			// j/w ale z col-begin/end
+			str = str.replace(/\n\{\{col-begin\}\}\n\s*(<tab<[0-9]+>tab>)\s*(?:\||\{\{col-(?:break|2)\}\})\s*(<tab<[0-9]+>tab>)\s*\{\{col-end\}\}/g
+				, '\n<div class="wikiflex">\n$1\n$2\n</div>'
+			);
+			
 			str = tables.show(str);
 
 			return str;
