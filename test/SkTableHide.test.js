8f01335012068d1223bd22e799904fa8451a6caf
Nux <eccenux+git@gmail.com>
2023-06-11 22:25:03 +0200

    sktable mocha tests

--- test/SkTableHide.test.js
+++ test/SkTableHide.test.js
@@ -1,48 +1,74 @@
+/* eslint-disable array-bracket-newline */
+/* global require, describe, it */
+const { assert } = require('chai');
+const bot = require('../SkTableHide');
+const SkTableHide = bot.SkTableHide;
 
 /**
  * SkTableHide tests
  */
-var tables = new SkTableHide();
+describe('SkTableHide', function () {
 
-var res;
+	var tables = new SkTableHide();
 
-// test indexes
-res = tables.findAll('abcabcaaaba', 'a');
-console.log(res);
+	describe('findAll', function () {
 
-// test hide
-var str = `
-before
-{|
-|
-{| class="wikitable"
-| abc
-|}
-|
-{| class="wikitable"
-| def
-|}
-|}
-after
-`;
-res = tables.hide(str);
-console.log('hide', res);
-// test undo
-var resShow = tables.show(res);
-console.log('resShow', resShow);
-if (resShow === str) {
-	console.log('OK :)');
-} else {
-	console.error('Not the same!', {resShow, str});
-}
+		it('find all letters', function () {
+			var res, expected;
 
-// test conditional show
-var resPartial = tables.showIf(res, (text) => text.search(/^\{\|.+class.+wikitable/) < 0);
-console.log('resPartial', resPartial);
-var resFull = tables.show(resPartial);
-console.log('resFull', resFull);
-if (resFull === str) {
-	console.log('OK :)');
-} else {
-	console.error('Not the same!', {resFull, str});
-}
+			// test indexes
+			res = tables.findAll('abcabcaaaba', 'a');
+			console.log(res);
+			expected = [ 0, 3, 6, 7, 8, 10 ];
+			assert.deepEqual(res, expected);
+		});
+	});
+
+	describe('show-hide', function () {
+		var str = `
+			before
+			{|
+			|
+			{| class="wikitable"
+			| abc
+			|}
+			|
+			{| class="wikitable"
+			| def
+			|}
+			|
+			{| class="sortable"
+			| def
+			|}
+			|}
+			after
+		`.replace(/\n\t+/g, '\n');
+
+		it('must hide and show', function () {
+			var res;
+			// test hide
+			res = tables.hide(str);
+			console.log('hide', res);
+			// only 2 tables because by default only wikitable is hidden
+			assert.isTrue(tables.findAll(res, '<tab<').length === 2, 'must hide 2 tables');
+			// test undo
+			var resShow = tables.show(res);
+			console.log('resShow', resShow);
+			assert.equal(resShow, str, 'must undo to the same wikitext');
+		});
+		it('test conditional show', function () {
+			var res;
+			res = tables.hide(str);
+			console.log('hide', res);
+			assert.isTrue(tables.findAll(res, '<tab<').length === 2, 'must hide 2 tables');
+			// show table with "def" which should show one of wikitables
+			var resPartial = tables.showIf(res, (text) => text.indexOf('def') >= 0);
+			console.log('resPartial', resPartial);
+			assert.isTrue(tables.findAll(resPartial, '<tab<').length === 1, 'must hide 1 table');
+			// should still show the last hidden table
+			var resFull = tables.show(resPartial);
+			console.log('resFull', resFull);
+			assert.equal(resFull, str, 'must be the same');
+		});
+	});
+});
