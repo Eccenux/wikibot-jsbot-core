6a8a34e1dc04d952319456d428d962a689525c90
eccenux <eccenux+git@gmail.com>
2023-07-22 16:57:35 +0200

    podmiana [[Morningside Arena]]

--- src/NuxJsBot.js
+++ src/NuxJsBot.js
@@ -127,12 +127,12 @@ class NuxJsBot {
 			/**/
 			// old link
 			after = str
-				.replace(/autor link *= *Adam Przybylski *(\|)/g, 'autor link = Adam Przybylski (1896–1945)$1')
-				// .replace(/\[\[Adam Przybylski(\||\]\])/g, '[[Adam Przybylski (1896–1945)$1')
-				// .replaceAll('[[Adam Przybylski (1896–1945)]]', '[[Adam Przybylski (1896–1945)|Adam Przybylski]]')
+				// .replace(/autor link *= *Adam Przybylski *(\|)/g, 'autor link = Adam Przybylski (1896–1945)$1')
+				.replaceAll('[[Morningside Arena]]', '[[Leicester Arena]]')
+				.replaceAll('[[Morningside Arena|', '[[Leicester Arena|')
 			;
 			if (after !== str) {
-				summary.push('Przybylski link [[WP:ZDBOT]]');
+				summary.push('Leicester Arena');
 				str = after;
 			}
 			/**
